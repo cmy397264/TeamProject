1d0d55086f0a656882dd3f7cf8443b98cf67d54d
cmy397264 <cmy397264@naver.com>
2025-03-22 10:57:05 +0900

    bug fix : 앱 초기화면에서 화면 회전 시 단계가 초기화되는 문제점 수정

--- app/src/main/java/com/example/businessreportgenerator/presentation/navigation/AppEntryPoint.kt
+++ app/src/main/java/com/example/businessreportgenerator/presentation/navigation/AppEntryPoint.kt
@@ -1,6 +1,7 @@
 package com.example.businessreportgenerator.presentation.navigation
 
 import android.content.Context
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
