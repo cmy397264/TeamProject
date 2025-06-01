166940e7c96e80e0afe39add7316c5f2bb7a1fbc
Hw0o0c <Hw0o0c@github.com>
2025-06-01 16:53:36 +0900

    refactor : Report 요청을 앱 시작 시 실행에서 제외하고 따로 작동하게 만듦.

--- app/src/main/java/com/bigPicture/businessreportgenerator/presentation/navigation/AppEntryPoint.kt
+++ app/src/main/java/com/bigPicture/businessreportgenerator/presentation/navigation/AppEntryPoint.kt
@@ -1,6 +1,8 @@
 package com.bigPicture.businessreportgenerator.presentation.navigation
 
+import android.content.SharedPreferences
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,11 +25,14 @@ import com.bigPicture.businessreportgenerator.presentation.features.analyst.Anal
 import com.bigPicture.businessreportgenerator.presentation.features.news.NewsViewModel
 import com.bigPicture.businessreportgenerator.presentation.onboarding.OnboardingScreen
 import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
 import org.koin.androidx.compose.koinViewModel
 import java.time.LocalDate
 
@@ -37,22 +42,24 @@ import java.time.LocalDate
 @Composable
 fun AppEntryPoint() {
     val context = LocalContext.current
-    val apiViewModel: ApiViewModel = viewModel()
+    val coroutineScope = rememberCoroutineScope()
+    var onboardingCompleted by remember { mutableStateOf<Boolean?>(null) }
+
     val newsViewModel: NewsViewModel = viewModel()
-    val stockViewModel: StockViewModel = koinViewModel()
-    val analystViewModel: AnalystViewmodel = koinViewModel()
+    val apiViewModel: ApiViewModel = viewModel()
     val apiState by apiViewModel.state.collectAsState()
 
-    val coroutineScope = rememberCoroutineScope()
-
-    var onboardingCompleted by remember { mutableStateOf<Boolean?>(null) }
+    val stockViewModel: StockViewModel = koinViewModel()
+    val analystViewModel: AnalystViewmodel = koinViewModel()
 
     LaunchedEffect(Unit) {
         val prefs = context.getSharedPreferences("user_prefs", 0)
         onboardingCompleted = prefs.getBoolean("onboarding_completed", false)
 
         apiViewModel.updateApiMessage("서버와의 연결을 확인하는 중...")
-        val connected = apiViewModel.sendPing()
+        val connected = withContext(Dispatchers.IO) {
+            apiViewModel.sendPing()
+        }
         Log.d("bigPicture", "connected : $connected")
 
         if (connected) {
@@ -60,70 +67,67 @@ fun AppEntryPoint() {
             Log.d("bigPicture", "onboardingCompleted : $onboardingCompleted")
 
             if (onboardingCompleted == true) {
-                val uuid = prefs.getString("uuid", null)
-                var fcm = prefs.getString("fcm_token", null)
-                if (fcm == null) {
-                    FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            fcm = task.result
-                            prefs.edit {
-                                putString("fcm_token", fcm)
-                            }
-                            Log.d("bigPicture", "fcm token : $fcm.toString()")
-                        } else
-                            Log.d("bigPicture", "FCM 토큰 가져오기 실패")
-                    }
-                }
-                apiViewModel.updateApiMessage("토큰을 전송하는 중...")
-                Log.d("bigPicture", "uuid : $uuid")
-                apiViewModel.sendToken(
-                    FCMToken(
-                        uuid = uuid ?: "-1",
-                        fcmToken = fcm ?: "-1"
+                withContext(Dispatchers.IO) {
+                    val uuid = prefs.getString("uuid", null) ?: "-1"
+                    var fcm = getFcmTokenSuspend(prefs)
+                    apiViewModel.updateApiMessage("토큰을 전송하는 중...")
+                    Log.d("bigPicture", "uuid : $uuid")
+                    apiViewModel.sendToken(
+                        FCMToken(
+                            uuid = uuid,
+                            fcmToken = fcm
+                        )
                     )
-                )
-
-                val today = LocalDate.now()
-                val todayWeek = today.dayOfWeek.value - 1
-                val reportDays = prefs.getString("report_days", null)
-                Log.d("bigPicture", "today : $todayWeek")
-                Log.d("bigPicture", "reportDays : $reportDays")
-                val reportDayList = reportDays?.split(",")?.map {
-                    it.toInt()
                 }
-                Log.d("bigPicture", "reportList : $reportDayList")
 
-                if (reportDayList?.contains(todayWeek) == true) {
-                    Log.d("bigPicture", "today is in reportList : ${reportDayList.contains(todayWeek)}")
-                    apiViewModel.updateApiMessage("레포트 정보를 갱신하는 중...")
+                launch(Dispatchers.IO) {
+                    val today = LocalDate.now()
+                    val todayWeek = today.dayOfWeek.value - 1
+                    val reportDayList = prefs.getString("report_days", null)
+                        ?.split(",")?.mapNotNull { it.toInt() }
+                    Log.d("bigPicture", "reportList : $reportDayList")
 
-                    val date = today.toString()
-                    val riskTolerance = prefs.getString("risk_tolerance", null).toString()
-                    val reportComplexity = prefs.getString("report_complexity", null).toString()
-                    val interests = prefs.getStringSet("interests", null)?.toList() ?: emptyList()
+                    if (reportDayList?.contains(todayWeek) == true) {
+                        Log.d(
+                            "bigPicture",
+                            "today is in reportList : ${reportDayList.contains(todayWeek)}"
+                        )
+                        apiViewModel.updateApiMessage("레포트 정보를 갱신하는 중...")
 
-                    stockViewModel.getLatestStocksGroupByDate().firstOrNull()?.let { stocks ->
-                        val filtered = stocks.filter { it.date != date }
-                        val deferred = filtered.map { stock ->
-                            val request = ReportRequest(
-                                reportType = "stock",
-                                stockName = stock.stockName,
-                                riskTolerance = riskTolerance,
-                                reportDifficultyLevel = reportComplexity,
-                                interestAreas = interests
-                            )
-                            coroutineScope.async {
-                                analystViewModel.requestReport(request)
-                                stockViewModel.updateStockDate(stock.stockName, date)
+                        val date = today.toString()
+                        val riskTolerance = prefs.getString("risk_tolerance", "") ?: ""
+                        val reportComplexity = prefs.getString("report_complexity", "") ?: ""
+                        val interests =
+                            prefs.getStringSet("interests", null)?.toList() ?: emptyList()
+
+                        stockViewModel.getLatestStocksGroupByDate().firstOrNull()?.let { stocks ->
+                            val filtered = stocks.filter { it.date != date }
+
+                            val deferred = filtered.map { stock ->
+                                val request = ReportRequest(
+                                    reportType = "stock",
+                                    stockName = stock.stockName,
+                                    riskTolerance = riskTolerance,
+                                    reportDifficultyLevel = reportComplexity,
+                                    interestAreas = interests
+                                )
+                                async {
+                                    analystViewModel.requestReport(request)
+                                    stockViewModel.updateStockDate(stock.stockName, date)
+                                }
                             }
+
+                            deferred.awaitAll()
+                            Toast.makeText(context, "레포트 갱신 완료", Toast.LENGTH_SHORT).show()
                         }
-                        deferred.awaitAll()
                     }
                 }
             }
 
             apiViewModel.updateApiMessage("환율 불러오는 중...")
-            newsViewModel.fetchExchange()
+            withContext(Dispatchers.IO) {
+                newsViewModel.fetchExchange()
+            }
             apiViewModel.updateApiMessage("로딩 완료")
             delay(500)
             apiViewModel.updateApiStatus(ApiStatus.DONE)
@@ -157,3 +161,19 @@ fun AppEntryPoint() {
         }
     }
 }
+
+suspend fun getFcmTokenSuspend(prefs: SharedPreferences): String =
+    suspendCancellableCoroutine { continuation ->
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val token = task.result
+                    prefs.edit() { putString("fcm_token", token) }
+                    Log.d("bigPicture", "fcm token : $token")
+                    continuation.resume(token, onCancellation = null)
+                } else {
+                    Log.d("bigPicture", "FCM 토큰 가져오기 실패: ${task.exception?.message}")
+                    continuation.resume("-1", onCancellation = null)
+                }
+            }
+    }
